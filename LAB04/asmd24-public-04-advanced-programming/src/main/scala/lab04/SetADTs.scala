84b9cd4a53c1c631b652e31f901a61cee2e91066
davidedimarco00 <davide.dimarco5@studio.unibo.it>
2026-07-10 13:03:18 +0200

    ADT-verifier finish task

--- LAB04/asmd24-public-04-advanced-programming/src/main/scala/lab04/SetADTs.scala
+++ LAB04/asmd24-public-04-advanced-programming/src/main/scala/lab04/SetADTs.scala
@@ -57,6 +57,94 @@ object SetADTs:
       def ===(other: Set[A]): Boolean =
         s.union(other).size() == s.size()
 
+  // Tree-based implementation using Binary Search Tree
+  object TreeSetADT extends SetADT:
+    
+    // Binary Search Tree data structure
+    enum Tree[A]:
+      case Empty()
+      case Node(value: A, left: Tree[A], right: Tree[A])
+    
+    import Tree.*
+
+    opaque type Set[A] = Tree[A]
+
+    def empty[A](): Set[A] = Empty()
+
+    extension [A](s: Set[A])
+      
+      def add(element: A): Set[A] = s match
+        case Empty() => Node(element, Empty(), Empty())
+        case Node(v, l, r) if v == element => s
+        case Node(v, l, r) => 
+          // We need ordering, but for testing purposes we can use hash codes
+          if element.hashCode() < v.hashCode() then
+            Node(v, l.add(element), r)
+          else
+            Node(v, l, r.add(element))
+
+      def contains(a: A): Boolean = s match
+        case Empty() => false
+        case Node(v, l, r) if v == a => true
+        case Node(v, l, r) =>
+          // Check both sides since hash collisions may cause elements on either side
+          l.contains(a) || r.contains(a)
+
+      def remove(a: A): Set[A] = s match
+        case Empty() => Empty()
+        case Node(v, l, r) if v == a =>
+          // Remove node: merge left and right subtrees
+          mergeTrees(l, r)
+        case Node(v, l, r) =>
+          // Need to check both sides due to potential hash collisions
+          val newLeft = l.remove(a)
+          val newRight = r.remove(a)
+          if (newLeft == l) && (newRight == r) then s
+          else Node(v, newLeft, newRight)
+
+      private def mergeTrees(t1: Tree[A], t2: Tree[A]): Tree[A] = (t1, t2) match
+        case (Empty(), t) => t
+        case (t, Empty()) => t
+        case (Node(v, l, r), t2) => Node(v, l, mergeTrees(r, t2))
+
+      private def appendSequences(s1: Sequence[A], s2: Sequence[A]): Sequence[A] = s1 match
+        case Nil() => s2
+        case Cons(h, t) => Cons(h, appendSequences(t, s2))
+
+      def toSequence(): Sequence[A] = s match
+        case Empty() => Nil()
+        case Node(v, l, r) =>
+          // In-order traversal
+          appendSequences(l.toSequence(), Cons(v, r.toSequence()))
+
+      def union(s2: Set[A]): Set[A] = s match
+        case Empty() => s2
+        case Node(v, l, r) =>
+          // Add current value to s2, then union left and right
+          l.union(r.union(s2.add(v)))
+
+      def intersection(s2: Set[A]): Set[A] =
+        // Convert to sequences, filter, and convert back
+        // This ensures correctness even with hash collisions
+        def fromSequence(seq: Sequence[A]): Set[A] = seq match
+          case Nil() => Empty()
+          case Cons(h, t) => fromSequence(t).add(h)
+        
+        def filterSeq(seq: Sequence[A], other: Set[A]): Sequence[A] = seq match
+          case Nil() => Nil()
+          case Cons(h, t) if other.contains(h) => Cons(h, filterSeq(t, other))
+          case Cons(_, t) => filterSeq(t, other)
+        
+        fromSequence(filterSeq(s.toSequence(), s2))
+
+      def size(): Int = s match
+        case Empty() => 0
+        case Node(_, l, r) => 1 + l.size() + r.size()
+
+      def ===(other: Set[A]): Boolean =
+        // Two sets are equal if they have same size and union doesn't add elements
+        s.size() == other.size() && s.union(other).size() == s.size()
+
 
 @main def trySetADTModule =
   import SetADTs.*
--- LAB04/asmd24-public-04-advanced-programming/src/test/scala/lab04/SetADTCheck.scala
+++ LAB04/asmd24-public-04-advanced-programming/src/test/scala/lab04/SetADTCheck.scala
@@ -3,7 +3,7 @@ package scala.lab04
 import org.scalacheck.Prop.{forAll, propBoolean}
 import org.scalacheck.{Arbitrary, Gen, Properties}
 
-import scala.lab04.SetADTs.{BasicSetADT, SetADT}
+import scala.lab04.SetADTs.{BasicSetADT, SetADT, TreeSetADT}
 
 abstract class SetADTCheck(name: String) extends Properties(name):
   val setADT: SetADT
@@ -74,6 +74,76 @@ abstract class SetADTCheck(name: String) extends Properties(name):
     // remove(x, add(y, s)) = add(y, remove(x, s))  if x != y
     forAll: (s: Set[Int], x: Int, y: Int) =>
       (x != y) ==> (s.add(y).remove(x) === s.remove(x).add(y))
+
+  // ALGEBRAIC PROPERTIES
+
+  // Associativity of union: (A ∪ B) ∪ C = A ∪ (B ∪ C)
+  property("associativity of union") =
+    forAll: (s1: Set[Int], s2: Set[Int], s3: Set[Int]) =>
+      ((s1 || s2) || s3) === (s1 || (s2 || s3))
+
+  // Associativity of intersection: (A ∩ B) ∩ C = A ∩ (B ∩ C)
+  property("associativity of intersection") =
+    forAll: (s1: Set[Int], s2: Set[Int], s3: Set[Int]) =>
+      ((s1 && s2) && s3) === (s1 && (s2 && s3))
+
+  // Commutativity of intersection: A ∩ B = B ∩ A
+  property("commutativity of intersection") =
+    forAll: (s1: Set[Int], s2: Set[Int]) =>
+      (s1 && s2) === (s2 && s1)
+
+  // Idempotence of union: A ∪ A = A
+  property("idempotence of union") =
+    forAll: (s: Set[Int]) =>
+      (s || s) === s
+
+  // Idempotence of intersection: A ∩ A = A
+  property("idempotence of intersection") =
+    forAll: (s: Set[Int]) =>
+      (s && s) === s
+
+  // Identity for union: A ∪ ∅ = A
+  property("identity for union") =
+    forAll: (s: Set[Int]) =>
+      (s || empty()) === s
+
+  // CROSS-PROPERTY TESTS (relationships between union and intersection)
+
+  // Distributivity of union over intersection: A ∪ (B ∩ C) = (A ∪ B) ∩ (A ∪ C)
+  property("distributivity of union over intersection") =
+    forAll: (s1: Set[Int], s2: Set[Int], s3: Set[Int]) =>
+      (s1 || (s2 && s3)) === ((s1 || s2) && (s1 || s3))
+
+  // Distributivity of intersection over union: A ∩ (B ∪ C) = (A ∩ B) ∪ (A ∩ C)
+  property("distributivity of intersection over union") =
+    forAll: (s1: Set[Int], s2: Set[Int], s3: Set[Int]) =>
+      (s1 && (s2 || s3)) === ((s1 && s2) || (s1 && s3))
+
+  // Absorption laws: A ∪ (A ∩ B) = A
+  property("absorption law 1") =
+    forAll: (s1: Set[Int], s2: Set[Int]) =>
+      (s1 || (s1 && s2)) === s1
+
+  // Absorption laws: A ∩ (A ∪ B) = A
+  property("absorption law 2") =
+    forAll: (s1: Set[Int], s2: Set[Int]) =>
+      (s1 && (s1 || s2)) === s1
+
+  // Subset relationship between sets
+  property("subset relationship") =
+    forAll: (s1: Set[Int], s2: Set[Int]) =>
+      val intersection = s1 && s2
+      val union = s1 || s2
+      // intersection is always subset of both operands
+      ((intersection || s1) === s1) && ((intersection || s2) === s2) &&
+      // both operands are subsets of union
+      ((s1 && union) === s1) && ((s2 && union) === s2)
+
+  // Union contains both sets: A ⊆ (A ∪ B) and B ⊆ (A ∪ B)
+  property("union contains both operands") =
+    forAll: (s1: Set[Int], s2: Set[Int]) =>
+      val union_set = s1 || s2
+      (s1 && union_set) === s1 && (s2 && union_set) === s2
       
   
 
@@ -82,5 +152,11 @@ abstract class SetADTCheck(name: String) extends Properties(name):
 object BasicSetADTCheck extends SetADTCheck("SequenceBased Set"):
   val setADT: SetADT = BasicSetADT
 
-  @main def visuallingCheckArbitrarySets =
+  @main def visuallyCheckArbitrarySets =
     Range(0,20).foreach(i => println(summon[Arbitrary[setADT.Set[Int]]].arbitrary.sample))
+
+object TreeSetADTCheck extends SetADTCheck("TreeBased Set"):
+  val setADT: SetADT = TreeSetADT
+
+  @main def visuallyCheckArbitrarySetsTree =
+    Range(0,20).foreach(i => println(summon[Arbitrary[setADT.Set[Int]]].arbitrary.sample.map(_.toSequence())))
